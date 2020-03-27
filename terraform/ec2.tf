906464b5b5bc4a4dd2e40d33470314633cf6bb36
naveenthangella <naveenthangella@gmail.com>
2020-03-27 15:17:13 -0700

    Ansible sample

---
+++ terraform/ec2.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "node1" {
+  ami = "ami-0777ff5c030fe1d38"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0183b01f41709052a"]
+  tags = {
+    name = "Node1"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami = "ami-0777ff5c030fe1d38"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0183b01f41709052a"]
+  tags = {
+    name = "Node2"
+  }
+}
