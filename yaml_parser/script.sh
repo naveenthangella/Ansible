2daef44d43acc395e90bd803bc3661652ff112aa
naveenthangella <naveenthangella@gmail.com>
2020-04-02 16:32:24 -0700

    Ansible sample

--- yaml_parser/script.sh
+++ yaml_parser/script.sh
@@ -15,6 +15,6 @@ while [ $COURSES_COUNT -gt 0 ]; do
 
   echo -e "Hello, I'm $NAME from $PLACE. My date of birth is $DATE. I'm learning $COURSES[$1]"
   COURSES_COUNT=$(($COURSES_COUNT-1))
-  i=$i+1
+  i=$($i+1)
 
 done
