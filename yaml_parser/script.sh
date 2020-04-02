919b4614f1a61432796eb2af3d769419f8d8b064
naveenthangella <naveenthangella@gmail.com>
2020-04-01 23:05:31 -0700

    Ansible sample

--- yaml_parser/script.sh
+++ yaml_parser/script.sh
@@ -5,4 +5,10 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
+COURSES_COUNT=$(cat sample.yml | yq . | jq '.courses[].name' | wc -l)
+
+while [ $COURSES_COUNT -gt 0 ]; do
+
 echo -e "Hello, I'm NAME from PLACE. My date of birth is DATE. List of course COURSES"
+
+done
