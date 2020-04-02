b2a0ed6fc7da1a07251bc2c29ebb207e8ccc05d6
naveenthangella <naveenthangella@gmail.com>
2020-04-02 16:35:18 -0700

    Ansible sample

--- yaml_parser/script.sh
+++ yaml_parser/script.sh
@@ -13,8 +13,8 @@ COURSES=$(cat sample.yml | yq . | jq '.courses[].name')
 
 while [ $COURSES_COUNT -gt 0 ]; do
 
-  echo -e "Hello, I'm $NAME from $PLACE. My date of birth is $DATE. I'm learning $COURSES[$1]"
+  echo -e "Hello, I'm $NAME from $PLACE. My date of birth is $DATE. I'm learning ${COURSES[$1]}"
   COURSES_COUNT=$(($COURSES_COUNT-1))
-  i=$(($i+1))
+  i=${($i+1)}
 
 done
